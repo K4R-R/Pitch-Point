e757593b8f5cde07f293faa0d145929c85d16299
K4R-R <karanr4ghu@gmail.com>
2024-07-11 18:46:44 +0530

    url changed

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const chatRoutes = require('./routes/chatRoutes');
 
 const io = require('socket.io')(http, {
    cors: {
-     origin: ["https://pitch-point.netlify.app","wss://pitch-point-frontend.onrender.com"],
+     origin: ["https://pitch-point.netlify.app","https://pitch-point-frontend.onrender.com"],
      methods: ["GET", "POST"]
    }
  });
